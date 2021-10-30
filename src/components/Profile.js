dab8aa1ae373e1cf2044f252dbfc028745ffb0ac
NoyNoya <noya.dolev@gmail.com>
2021-10-30 16:33:10 +0300

    add icons to profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,9 @@
-import React,{ useEffect, useState, Component } from "react";
+import React,{ useEffect, useState } from "react";
 import { logOut, auth, fetchUser } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import './Profile.css';
+import { Home, Cake, AlternateEmail, Edit} from '@material-ui/icons';
 
 export default function Profile() {
     const [extendedUserInfo, setExtendedUserInfo] = useState(null);
@@ -31,18 +32,27 @@ export default function Profile() {
                     <div></div>
                 </div>
                 <div className="form-group">
-                    <label>Email address:</label>
+                <div className="icon-label-container">
+                        <AlternateEmail></AlternateEmail>
+                        <label>Email:</label>
+                    </div>
                     <div className="user-property-value">{user.email}</div>
                 </div>
 
                 <div className="form-group">
-                    <label>Birth Date:</label>
-                    <div className="user-property-value">{new Date(extendedUserInfo.birthDate.seconds * 1000).toDateString()}</div>
+                    <div className="icon-label-container">
+                        <Home></Home>
+                        <label>Address:</label>
+                    </div>
+                    <div className="user-property-value">{extendedUserInfo.address}</div>
                 </div>
 
                 <div className="form-group">
-                    <label>Address:</label>
-                    <div className="user-property-value">{extendedUserInfo.address}</div>
+                <div className="icon-label-container">
+                        <Cake></Cake>
+                        <label>Birth date:</label>
+                    </div>
+                    <div className="user-property-value">{new Date(extendedUserInfo.birthDate.seconds * 1000).toDateString()}</div>
                 </div>
 
                 <div className="logout-button-container">
